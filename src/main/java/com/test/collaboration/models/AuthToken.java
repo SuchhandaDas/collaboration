acd6ff2674bf46e5f2f2954f3d8c24f7e968e6cb
suchhanda <dassuchhanda5@gmail.com>
2025-04-11 11:19:14 +0530

    Add model for employee authentication

---
+++ src/main/java/com/test/collaboration/models/AuthToken.java
@@ -0,0 +1,22 @@
+package com.test.collaboration.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthToken {
+    private String token;
+    private long employeeId;
+    private LocalDateTime expiry;
+
+    boolean isExpired() {
+        return expiry.isBefore(LocalDateTime.now());
+    }
+}
